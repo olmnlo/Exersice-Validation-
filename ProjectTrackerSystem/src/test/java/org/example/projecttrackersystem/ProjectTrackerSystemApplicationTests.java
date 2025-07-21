37f5acf17c5770267bc938257c8ee85f0f1ff6e3
olmnlo <hosamag54@gmail.com>
2025-07-21 16:58:53 +0300

    created project tracker

---
+++ ProjectTrackerSystem/src/test/java/org/example/projecttrackersystem/ProjectTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projecttrackersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
